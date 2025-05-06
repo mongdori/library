dcd01dcb86980d61e525aa6316827a2a4cc14a72
mongdori <mongdori00@icloud.com>
2025-05-06 23:24:21 +0900

    ㅕãUser constructor

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,8 +1,13 @@
 package entity;
 
-
 public class User {
 
-    private Long id;
-    private String user;
+    private final Long id;
+    private final String user;
+
+    public User(Long id, String user) {
+        this.id = id;
+        this.user = user;
+    }
 }
+
